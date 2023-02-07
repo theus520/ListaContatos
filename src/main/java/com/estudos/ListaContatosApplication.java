b27783ddded91546e7c6fd7ca9bfbf1ab130e685
Matheus rabelo <matheusrabelo520@gmail.com>
2023-02-06 22:01:25 -0300

    Iniciado o projeto

---
+++ src/main/java/com/estudos/ListaContatosApplication.java
@@ -0,0 +1,13 @@
+package com.estudos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaContatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaContatosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudos/ListaContatosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaContatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
