faef892cde0509906cb9070644b6d38883914a8f
Matheus rabelo <matheusrabelo520@gmail.com>
2023-03-04 10:02:04 -0300

    Configurando o armazenamento de contatos

---
+++ src/main/java/com/estudos/Contato.java
@@ -0,0 +1,49 @@
+package com.estudos;
+
+public class Contato {
+	
+	public Contato() {
+		
+	}
+
+	public Contato(String id, String nome, String telefone) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.telefone = telefone;
+	}
+
+	private String id;
+	
+	private String nome;
+	
+	private String telefone;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+	
+	
+}
--- src/main/java/com/estudos/ContatosControle.java
+++ src/main/java/com/estudos/ContatosControle.java
@@ -1,11 +1,24 @@
 package com.estudos;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ContatosControle {	
-	
+public class ContatosControle {
+
+	private static final ArrayList<Contato> LISTA_CONTATOS = new ArrayList<>();
+
+	static {
+
+		LISTA_CONTATOS.add(new Contato("1", "Matheus", "1234765432"));
+		LISTA_CONTATOS.add(new Contato("2", "Marcos", "1234765432"));
+		LISTA_CONTATOS.add(new Contato("3", "Junior", "1234765432"));
+		LISTA_CONTATOS.add(new Contato("4", "Marcelo", "1234765432"));
+
+	}
+
 	@GetMapping("/")
 	public String index() {
 		return "index";
